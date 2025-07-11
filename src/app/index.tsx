5667c5712bbe543ca9617d31dd984144585c15f2
Leolfc <leonardofeliciano81@gmail.com>
2025-07-11 14:16:59 -0300

    adicionando novas funcionalidades

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,9 @@
-import { Category } from "@/components/category";
+import { Categories } from "@/components/categories";
+import { Link } from "@/components/links";
 import { colors } from "@/styles/colors";
 import { MaterialIcons } from "@expo/vector-icons"; //*importando o MaterialIcons do expo*/
-import { Image, TouchableOpacity, View } from "react-native";
-import { styles } from "./styles";
+import { FlatList, Image, TouchableOpacity, View, Modal, Text } from "react-native";
+import { styles } from "./styles"; //*importando os estilos do arquivo styles.ts
 export default function () {
   return (
     <View style={styles.container}>
@@ -13,10 +14,41 @@ export default function () {
           <MaterialIcons name="add" size={32} color={colors.green[300]} />
         </TouchableOpacity>
       </View>
+      <Categories />
 
-      <Category name="Projetos" icon="code" isSelected  />
-       <Category name="Site" icon="language" isSelected={false} />
-       <Category name="Video" icon="movie" isSelected={false} />
+      <FlatList //*FlatList é um componente que renderiza uma lista de itens de forma eficiente
+        data={["1", "2", "3", ]}
+        keyExtractor={(item) => item}
+        renderItem={() => (
+          <Link
+            name="Rockseat"
+            url="https://www.rocketseat.com.br/"
+            onDetails={() => console.log("clicou!!!")}
+         
+           />
+        )}
+        style={styles.links}
+        contentContainerStyle={styles.linksContent}
+        showsVerticalScrollIndicator={false}
+      />
+      <Modal transparent visible={true}>
+        <View style={styles.modal}>
+        <View style={styles.modalContent}>
+          <View style={styles.modalHeader}>
+        <Text style={styles.modalCategory}>Curso</Text>
+       <TouchableOpacity>
+        <MaterialIcons name="close" size={20} color={colors.gray[400]}/>
+       </TouchableOpacity>
+        
+          </View>
+          <Text style={styles.modalLinkName}>Rockseat</Text>
+          <Text style={styles.modalUrl}>
+            https://www.rocketseat.com.br/
+          </Text>
+        </View>
+        </View>
+      </Modal>
     </View>
+  
   );
 }
--- src/components/category/styles.ts
+++ src/components/category/styles.ts
@@ -1,10 +1,5 @@
 import { StyleSheet } from "react-native";
 
-
-
-
-
-
 export const styles = StyleSheet.create({ //Estilizações
 container:{
     flexDirection: 'row',
